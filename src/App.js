3fb7272a29c4e65d53509559e5e9b7ed9125dd58
randilwelikala <randilgimantha646@gmail.com>
2025-03-16 23:38:15 +0530

    display gender n homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [gender,setGender] = useState('');
   const pronunList ={
     'Mr':'Male',
     'Ms':'Feamle',
     'Miss':'Female'
   };
+
+  const handlePronounChange = (event)=>{
+    const selectedPronoun = event.target.value;
+    setGender(selectedPronoun);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <lable htmlFor='pronoun'>Select Pronoun</lable>
-        <select key={'pronoun'}>
+        <select key={'pronoun'} onChange={handlePronounChange}>
+        <option value="">Select your pronoun</option>
           <option value="Mr">Mr</option>
           <option value="Ms">Ms</option>
           <option value="Miss">Miss</option>
@@ -31,6 +39,7 @@ function App() {
           <lable>Female</lable>
           <input value='Female' type='radio' name='gender'></input>
         </div>
+        <p>{gender}</p>
 
         
 
