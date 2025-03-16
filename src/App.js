3ae30804761d0b7162509f3ae6155a4095f93303
randilwelikala <randilgimantha646@gmail.com>
2025-03-16 23:28:41 +0530

    create form

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const pronunList ={
+    'Mr':'Male',
+    'Ms':'Feamle',
+    'Miss':'Female'
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <lable htmlFor='pronoun'>Select Pronoun</lable>
+        <select key={'pronoun'}>
+          <option value="Mr">Mr</option>
+          <option value="Ms">Ms</option>
+          <option value="Miss">Miss</option>
+
+        </select>
+        <br/>
+
+        <input typr='text' placeholder ='First Name'/>
+        <input typr='text' placeholder ='Last Name'/> 
+        
+        <div>
+          <lable>Male</lable>
+          <input value='Male' type='radio' name='gender'></input>
+        </div>
+
+        <div>
+          <lable>Female</lable>
+          <input value='Female' type='radio' name='gender'></input>
+        </div>
+
+        
+
+
       </header>
     </div>
   );
